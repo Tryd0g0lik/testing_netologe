def562a6816fa655be1f603b9984a24cc2f55dd9
Tryd0g0lik <work80@mail.ru>
2022-08-26 10:16:18 +0700

    README file added

--- yandex_folder/yandex_api.py
+++ yandex_folder/yandex_api.py
@@ -3,7 +3,7 @@ import requests
 import json
 import urllib.parse as parse
 from yandex_folder.yand_token import token_key
-from  decorate import *
+from yandex_folder.decorate import *
 
 
 path = 'disk:/foo' # путь к корневой папке
@@ -31,14 +31,14 @@ def yandex_requet_get_disk(folder, token, header): # check the yandex-disk on th
 # print(f)
 
 path_in_root = "подпись_в_цифре"
-folder_of_putting = "xxx2"
+folder_of_putting = "xxx10"
 @parametrized_decoder(path_to_log = "files/log2.txt")
 def yandex_request_put_foldr(path_in_root, folder_of_putting, header):
 
   url = "https://cloud-api.yandex.net/v1/disk/resources"
   parametres = {'path' : "%s/%s" % (path_in_root, folder_of_putting)}
   response = requests.put(url, params=parametres, headers=header)
-  return response
+  return [response.status_code]
 
 t = yandex_request_put_foldr(path_in_root, folder_of_putting, header)
-print(type(t))
+print(t)
