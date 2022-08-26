8a1eb2840d3febceb07591ad9cbae1262157485f
Tryd0g0lik <work80@mail.ru>
2022-08-26 10:13:15 +0700

    README file added

--- yandex_folder/test.py
+++ yandex_folder/test.py
@@ -3,6 +3,7 @@ import pytest
 # from yandex_api import yandex_request_put_foldr
 from yandex_folder.yand_token import token_key
 from yandex_folder.yandex_api import yandex_request_put_foldr
+from yandex_folder.decorate import parametrized_decoder
 
 token = token_key()
 
@@ -15,11 +16,11 @@ header = {
 # folder_of_putting = "xxx2"
 
 parametre = [
-  ("подпись_в_цифре", "xxx7", header, "<Response [201]>"),
-  ("подпись_в_цифре", "xxx2", header, "<Response [409]>"),
-  ("подпись_в_цифрЕ", "xxx4", header, "<Response [409]>"),
-  ("Подпись_в_цифре", "xxx5", header, "<Response [409]>"),
-  ("подписьв_цифре", "xxx6", header, "<Response [409]>")
+  ("подпись_в_цифре", "xxx7", header, f"{(201,)}"),
+  ("подпись_в_цифре", "xxx2", header, f"{(409,)}"),
+  ("подпись_в_цифрЕ", "xxx4", header, f"{(409,)}"),
+  ("Подпись_в_цифре", "xxx5", header, f"{(409,)}"),
+  ("подписьв_цифре", "xxx6", header, f"{(409,)}")
 ]
 
 @pytest.mark.parametrize("path_in_root, folder_of_putting, header, result", parametre)
